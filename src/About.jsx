3ef91405e1f9ae4ef0ddfb3bb745b5758d376934
Rakesh <rakeshsaharan961011@gmail.com>
2021-10-14 12:55:53 +0530

    my first commit reactwebsite

---
+++ src/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Common from './Common';
+import web from "../src/images/img3.png";
+
+const About = () => {
+  return (
+    <>
+      <Common name='Welcome to About page' imgsrc={web} visit="/contact" btname="Contact Now" />
+
+    </>
+  );
+};
+
+export default About;
---
+++ src/Sdata.jsx
@@ -0,0 +1,35 @@
+import web from "../src/images/web.jpg";
+import app from "../src/images/app.jpg";
+import software from "../src/images/software.jpg";
+import android from "../src/images/android.jpg";
+import digital from "../src/images/img1.jpg";
+import marketing from "../src/images/market.jpg";
+
+const Sdata = [
+    {
+        imgsrc: web,
+        title: "web development"
+    },
+    {
+        imgsrc: app,
+        title: "app development"
+    },
+    {
+        imgsrc: software,
+        title: "software development"
+    },
+    {
+        imgsrc: digital,
+        title: "digi development"
+    },
+    {
+        imgsrc: marketing,
+        title: "marketing development"
+    },
+    {
+        imgsrc: android,
+        title: "android development"
+    },
+];
+
+export default Sdata;
